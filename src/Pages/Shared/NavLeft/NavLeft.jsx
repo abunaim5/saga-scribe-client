418fda937efb67f4ab43eadcd3f5134a6c4308da
abunaim5 <abunaim140@gmail.com>
2024-05-10 23:00:01 +0600

    added left nev social icons

--- src/Pages/Shared/NavLeft/NavLeft.jsx
+++ src/Pages/Shared/NavLeft/NavLeft.jsx
@@ -1,7 +1,15 @@
+import { FaFacebookF, FaInstagram, FaTwitter, FaYoutube, FaSearch } from "react-icons/fa";
 
 const NavLeft = () => {
     return (
-        <div className="w-[5%] flex items-center justify-center">
+        <div className="w-[5%] flex items-center flex-col justify-center gap-14">
+            <div className="flex flex-col items-center justify-center gap-6">
+                <button><a href=""><FaFacebookF /></a></button>
+                <button><a href=""><FaInstagram /></a></button>
+                <button><a href=""><FaTwitter /></a></button>
+                <button><a href=""><FaYoutube /></a></button>
+                <button><a href=""><FaSearch /></a></button>
+            </div>
             <button className="font-bold -rotate-90">SUBSCRIBE</button>
         </div>
     );
