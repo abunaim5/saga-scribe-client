cbbcc1760e49da3312f6dc7570ec035fca706a4d
abunaim5 <abunaim140@gmail.com>
2024-05-11 15:45:22 +0600

    added some styles in recent blogs and card component

--- src/Pages/Home/RecentBlogs/RecentBlogCard.jsx
+++ src/Pages/Home/RecentBlogs/RecentBlogCard.jsx
@@ -6,15 +6,20 @@ const RecentBlogCard = ({ blog }) => {
 
     return (
         <Card
+            
             // onClick={() => setIsOpen(true)}
-            className="rounded-none rounded-t-none shadow-none border-none"
+            className="rounded-none rounded-t-none shadow-none border-none overflow-hidden"
             imgAlt="Meaningful alt text for an image that is not purely decorative"
             // imgSrc={image}
-            renderImage={() => <div style={{ backgroundImage: `url(${image})` }} className="min-h-[478px] bg-cover bg-no-repeat object-contain bg-center">
-                <div className="w-full h-full flex items-center justify-center flex-col bg-black bg-opacity-40 gap-5">
+            renderImage={() => <div style={{ backgroundImage: `url(${image})` }} className="min-h-[478px] bg-cover  bg-center transition-transform duration-1000 ease-in-out hover:scale-105 relative">
+                <div className="w-full h-full flex items-center justify-center flex-col bg-black bg-opacity-40 transition-all duration-1000 ease-in-out hover:bg-opacity-50 gap-5">
                     <h4 className="text-sm font-semibold text-white uppercase">{category} / {read_time}</h4>
                     <h3 className="text-3xl font-bold text-white uppercase">{title}</h3>
                     <p className="text-white italic font-medium">{story_theme}</p>
+                    {/* <div className="flex gap-4 absolute bottom-0 w-full">
+                        <Button className="w-full rounded-sm uppercase">Details</Button>
+                        <Button className="w-full rounded-sm uppercase">Wishlist</Button>
+                    </div> */}
                 </div>
 
             </div>}
--- src/Pages/Home/RecentBlogs/RecentBlogs.jsx
+++ src/Pages/Home/RecentBlogs/RecentBlogs.jsx
@@ -20,7 +20,7 @@ const RecentBlogs = () => {
             <div className="flex items-center justify-center border-2 border-black h-20">
                 <h2 className="text-3xl font-bold uppercase">Recent Blogs</h2>
             </div>
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-2">
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-2 px-2">
                 {
                     blogs.map((blog, idx) => <RecentBlogCard
                         key={idx}
